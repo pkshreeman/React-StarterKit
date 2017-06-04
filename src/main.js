56d67c83b40d59eda110cbc90616a56f63a7f2b6
Dr. Paul Kenneth Shreeman <pkshreeman@gmail.com>
2017-06-03 20:51:46 -0700

    Adding my own code
    Running some testing for creating my own React app.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
- 
+import $ from 'jquery';
+
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
     React.createElement(Counter),
@@ -9,3 +10,15 @@ document.addEventListener('DOMContentLoaded', function() {
   );
 });
 console.log('Hello World!');
+
+//{"username":"sjames1958gm","img":"https://avatars.githubusercontent.com/u/4639625?v=3","alltime":7225,"recent":510,"lastUpdate":"2017-05-31T10:27:51.438Z"} - Expected Response Type
+// Just setting up global variables here.
+var allTimeUrl = "https://fcctop100.herokuapp.com/api/fccusers/top/alltime"
+var last30Days = "https://fcctop100.herokuapp.com/api/fccusers/top/recent"
+var allTimeResponse = {}
+var recentResponse = {}
+
+function grabData(url, data){
+  $.getJSON(url).done(response =>{data = response; console.log(data)})
+}
+grabData(allTimeUrl, allTimeResponse)
